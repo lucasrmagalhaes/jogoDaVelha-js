f3cab45675706deb020380a7b993dbdb5ad03064
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 09:41:16 -0300

    x nos quadrados funcionando

--- velha.js
+++ velha.js
@@ -1,5 +1,16 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado');
+
+mudarJogador('X');
 
 function escolherQuadrado(id) {
-    console.log(id)
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
