1344eb2889bc8363ea6efb0b601091f71ac44c9f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 09:53:50 -0300

    checando o vencedor

--- velha.js
+++ velha.js
@@ -1,10 +1,15 @@
 var jogador = null;
 var jogadorSelecionado = document.getElementById('jogador-selecionado');
+var quadrados = document.getElementsByClassName('quadrado');
 
 mudarJogador('X');
 
 function escolherQuadrado(id) {
     var quadrado = document.getElementById(id);
+
+    if(quadrado.innerHTML !== '-') {
+        return;
+    }
 
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#000';
@@ -20,4 +25,16 @@ function escolherQuadrado(id) {
 function mudarJogador(valor) {
     jogador = valor;
     jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor() {
+    var quadrado1 = document.getElementById(1);
+    var quadrado2 = document.getElementById(2);
+    var quadrado3 = document.getElementById(3);
+    var quadrado4 = document.getElementById(4);
+    var quadrado5 = document.getElementById(5);
+    var quadrado6 = document.getElementById(6);
+    var quadrado7 = document.getElementById(7);
+    var quadrado8 = document.getElementById(8);
+    var quadrado9 = document.getElementById(9);
 }
